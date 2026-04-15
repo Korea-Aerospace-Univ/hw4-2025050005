71d6af00b1763649e71bde2452c8ecbe6ffe7951
2025050005 <inhyang227@gmail.com>
2026-04-15 16:15:38 +0900

    hw1.c

--- p1/main.c
+++ p1/main.c
@@ -5,21 +5,20 @@ int main()
     int i=0, a=0, g=0;
     scanf("%d\n", &a);
     do {
+        i+=1;
         scanf("%d", &g);
         if (a>g){
             printf("%d<?\n", g);
-            i+=1;
         }
         else if (a<g){
             printf("%d>?\n", g);
-            i+=1;
         }
         else{
             printf("%d==?\n", g);
-            printf("%d",i+1);
-            break;
+            printf("%d",i);
         }
     } while (a!=g);
-
     return 0;
 }
+
+
